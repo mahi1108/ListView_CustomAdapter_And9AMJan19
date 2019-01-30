8fcc9244afeae4c80d2abbf6fcbdaa4123c3e19a
maheshthippala <mahesh.cubex@gmail.com>
2019-01-30 10:23:05 +0530

    Added WhatsApp Path & Runtime permission

--- app/src/main/java/cubex/mahesh/listview_and9amjan19/MainActivity.kt
+++ app/src/main/java/cubex/mahesh/listview_and9amjan19/MainActivity.kt
@@ -1,8 +1,13 @@
 package cubex.mahesh.listview_and9amjan19
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
@@ -11,14 +16,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        readFiles()
+
+        var status = ContextCompat.checkSelfPermission(
+            this@MainActivity,
+            Manifest.permission.READ_EXTERNAL_STORAGE)
+        if(status == PackageManager.PERMISSION_GRANTED) {
+            readFiles()
+        }else{
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                123)
+        }
     }  // onCreate
 
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+        {
+            readFiles()
+        }else{
+            Toast.makeText(this@MainActivity,
+                "You can't read the with out storage permission ",
+                Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+
+
     fun readFiles( ){
-        var path = "/storage/sdcard0/"
+        var path = "/storage/sdcard0/GBWhatsApp/Media/GBWhatsApp Images/Sent/"
         var f = File(path)
         if(!f.exists()){
-            path = "/storage/emulated/0/"
+            path = "/storage/emulated/0/GBWhatsApp/Media/GBWhatsApp Images/Sent/"
             f = File(path)
         }
         var files : Array<String>  = f.list()
